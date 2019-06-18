4eac26244cab34de151e2144465dfcf58c8e9b10
Kyle Barton <bartokyl000@gmail.com>
2019-06-18 20:24:50 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,7 +4,8 @@ puts "   |    |   "
 puts " -----------"
 puts "   |    |   "
 board = ["X", "O", " ", "X", "O", " ", "O", "X", " "]
-def display_board = board
+def display_board 
+  board
 end
 
   
